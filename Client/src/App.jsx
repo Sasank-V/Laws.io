81294e7320d62d0e250b75ba31e93bd92741bcc2
Sasank-V <sasanklearner@gmail.com>
2024-09-07 20:51:28 +0530

    Added MCQ Screen

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,10 @@
+import MCQ from "./screens/Mcq.jsx";
 function App() {
-
   return (
     <>
-
+    <div className="absolute w-full h-full">
+      <MCQ/>
+    </div>
     </>
   )
 }
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily:{
+      'inria' : ["Inria Sans", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
