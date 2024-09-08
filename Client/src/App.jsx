95a5a02fbe9c29bdd058521cb27860f71fd17d09
gkmohith17 <gkmohith17@gmail.com>
2024-09-08 17:44:05 +0530

    Case Study Screen Created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,7 +4,8 @@ import { useState } from "react"
 import StartPage from "./screens/StartPage.jsx";
 import McqPage from "./screens/McqPage";
 import LOTD from "./screens/LOTDPage.jsx";
-import SideBar from "./components/SideBar/SideBar.jsx"
+import SideBar from "./components/SideBar/SideBar.jsx";
+import CaseStudy from "./screens/CaseStudy.jsx";
 
 function App() {
   const [pageNumber, setPageNumber] = useState(0);
@@ -13,11 +14,12 @@ function App() {
   return (
     <div className="flex w-[100vw] h-[100vh] text-white justify-start">
       <SideBar pageNumber={pageNumber} setPageNumber={setPageNumber} navOpen={navOpen} setNavOpen={setNavOpen} />
-      <McqPage navOpen={navOpen} />
+      {/* <McqPage navOpen={navOpen} /> */}
       {/* <div className="absolute top-[10%] left-[10%] w-[85%] h-[80%] items-center transition-all duration-500 z-1"
           style={{ transform: show === false ? 'scale(1)' : 'scale(0)' }}>
           <LOTD setShow={setShow} />
           </div> */}
+          <CaseStudy/>
     </div >
   );
 }
