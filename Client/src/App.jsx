9fac0668c62f18d92bf985a168315f526ccbee15
Sooraj Sunil <sooraj.namboothiry@gmail.com>
2024-09-08 19:58:28 +0530

    Case Study ?

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,13 +5,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import StartPage from "./screens/StartPage.jsx";
 import MCQPage from "./screens/McqPage.jsx";
 import LOTD from "./screens/LOTDPage.jsx";
-<<<<<<< HEAD
 import SideBar from "./components/SideBar/SideBar.jsx";
 import CaseStudy from "./screens/CaseStudy.jsx";
-=======
-
-import SideBar from "./components/SideBar/SideBar.jsx"
->>>>>>> f7b9f83c35457277407c949e1297acef85b991af
 
 
 function App() {
@@ -19,17 +14,6 @@ function App() {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-<<<<<<< HEAD
-    <div className="flex w-[100vw] h-[100vh] text-white justify-start">
-      <SideBar pageNumber={pageNumber} setPageNumber={setPageNumber} navOpen={navOpen} setNavOpen={setNavOpen} />
-      {/* <McqPage navOpen={navOpen} /> */}
-      {/* <div className="absolute top-[10%] left-[10%] w-[85%] h-[80%] items-center transition-all duration-500 z-1"
-          style={{ transform: show === false ? 'scale(1)' : 'scale(0)' }}>
-          <LOTD setShow={setShow} />
-          </div> */}
-          <CaseStudy/>
-    </div >
-=======
     <div className="absolute w-full h-full">
       <div className="w-full h-full text-[#CCCCCC] font-inria md:overflow-hidden z-2 flex">
         <Router>
@@ -41,11 +25,11 @@ function App() {
             <Route path="/LOTD" element={<LOTD setPageNumber={setPageNumber} />} />
             {/* MCQ Quiz Page */}
             <Route path="/Quiz" element={<MCQPage />} />
+            <Route path="/Case" element={<CaseStudy />} />
           </Routes>
         </Router>
       </div>
     </div>
->>>>>>> f7b9f83c35457277407c949e1297acef85b991af
   );
 }
 
